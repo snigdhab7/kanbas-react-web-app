3f5d64f4922db1a6bbc5b96b9623cc05a54d8716
Snigdha Bose <62890614+snigdhab7@users.noreply.github.com>
2023-12-04 22:31:04 -0500

    a6

--- src/KanbasNavigation/index.js
+++ src/KanbasNavigation/index.js
@@ -35,10 +35,14 @@ return (
 
     {links.map((link,index) => (
         
-         <Link
+   /*       <Link
         key={index}
         to={`/Kanbas/${link.name}`}
-        > 
+        >  */
+        <Link
+  key={index}
+  to={link.name === "Account" ? `/Kanbas/account/:id` : `/Kanbas/${link.name}`}
+>
 
 
 
